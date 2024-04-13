1d549f1a8ec26c066e19e0bfe24e73cb52104236
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-04-13 15:39:30 +0000

    fixed Hydration failed because the initial UI does not match what was rendered on the server

--- containers/nextjs/front-end/app/login/page.tsx
+++ containers/nextjs/front-end/app/login/page.tsx
@@ -7,7 +7,9 @@ export default function Login() {
   return (
     <div className="flex flex-col mb-0 min-h-screen">
       <div className="flex items-center mobile:my-5 mobile:mx-5 desktop:my-16 desktop:mx-56 justify-between">
-        <img
+        <Image
+          width={200}
+          height={200}
           src="/leet-reviews-logo.svg"
           alt="leet-reviews-logo"
           className="mobile:h-5 desktop:h-8"
@@ -20,51 +22,57 @@ export default function Login() {
       </div>
       <div className="m-10 self-center flex flex-col desktop:gap-24 mobile:gap-28">
         <div className="mobile:w-96 mobile:my-10 desktop:w-100 desktop:-ml-12">
-          <p className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
+          <div className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
             <span className="text-primary font-semibold text-lg">
               Discover Real Insights:
             </span>
-            <p className="text-quaternary text-base">
+            <div className="text-quaternary text-base">
               Gain firsthand knowledge from your peers about their internship
               experiences, providing invaluable insights into various companies
               and industries.
-            </p>
-          </p>
-          <img
+            </div>
+          </div>
+          <Image
+            width={50}
+            height={50}
             src="/check-mark.svg"
             alt="check-mark"
             className="desktop:h-12 desktop:-my-5 desktop:-mx-5 desktop:-mt-36 mobile:h-10 mobile:-my-5 mobile:-mx-5 mobile:-mt-44"
           />
         </div>
         <div className="mobile:w-96 mobile:my-10 desktop:w-100">
-          <p className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
+          <div className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
             <span className="text-primary font-semibold text-lg">
               Empower Informed Decisions:
             </span>
-            <p className="text-quaternary text-base">
+            <div className="text-quaternary text-base">
               Make informed decisions about your future internships by accessing
               candid reviews and detailed accounts directly from students who
               have been there.
-            </p>
-          </p>
-          <img
+            </div>
+          </div>
+          <Image
+            width={50}
+            height={50}
             src="/check-mark.svg"
             alt="check-mark"
             className="desktop:h-12 desktop:-my-5 desktop:-mx-5 desktop:-mt-36 mobile:h-10 mobile:-my-5 mobile:-mx-5 mobile:-mt-44"
           />
         </div>
-        <div className="mobile:w-96 mobile:my-10 desktop:w-100 desktop:ml-28">
-          <p className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
+        <div className="mobile:w-96 mobile:my-10 desktop:w-100 desktop:ml-12">
+          <div className="bg-quinary p-5 rounded-xl hover:shadow-2xl shadow-lg">
             <span className="text-primary font-semibold text-lg">
               Community-Driven Resource:
             </span>
-            <p className="text-quaternary text-base">
+            <div className="text-quaternary text-base">
               Join a supportive community where students share their
               experiences, fostering transparency and helping you navigate the
               internship landscape with confidence.
-            </p>
-          </p>
-          <img
+            </div>
+          </div>
+          <Image
+            width={50}
+            height={50}
             src="/check-mark.svg"
             alt="check-mark"
             className="desktop:h-12 desktop:-my-5 desktop:-mx-5 desktop:-mt-36 mobile:h-10 mobile:-my-5 mobile:-mx-5 mobile:-mt-44"
@@ -73,16 +81,22 @@ export default function Login() {
       </div>
       <div className="flex flex-col justify-center items-center shadow-2xl rounded-lg mobile:w-96 desktop:w-30 p-6 self-center mt-52 gap-5">
         <div className="flex flex-col items-center text-quaternary">
-          <p className="font-bold mobile:text-xl desktop:text-2xl ">
+          <div className="font-bold mobile:text-xl desktop:text-2xl ">
             Share With Us Your Experience.
-          </p>
-          <p className="font-bold mobile:text-xl desktop:text-2xl ">
+          </div>
+          <div className="font-bold mobile:text-xl desktop:text-2xl ">
             Join NOW!
-          </p>
+          </div>
         </div>
         <Link href={"http://localhost:8000/42/oauth"}>
           <div className="flex justify-center items-center bg-primary font-bold text-white gap-3 w-64 p-2 rounded-xl hover:shadow-2xl hover:bg-quaternary">
-            <img src="/42-logo.svg" alt="42-logo" className="h-7" />
+            <Image
+              width={50}
+              height={50}
+              src="/42-logo.svg"
+              alt="42-logo"
+              className="h-7"
+            />
             <span>Sign in with Intra42</span>
           </div>
         </Link>
@@ -95,7 +109,9 @@ export default function Login() {
         </div>
         <span>LeetReviews 2024</span>
         <a href="https://github.com/yel-aziz/Reviews-Leet" target="_blank">
-          <img
+          <Image
+            width={50}
+            height={50}
             src="/github-logo.svg"
             alt="github-logo"
             className="mobile:h-5 desktop:h-10"
